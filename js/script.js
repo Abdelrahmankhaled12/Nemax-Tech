cd459ea42faa3e6a6a61413914ea2b486cc57f4c
AbdelrahmanKaled12 <abdelrahman.khaled1086@gmail.com>
2023-06-22 01:17:24 +0300

    up

---
+++ js/script.js
@@ -0,0 +1,41 @@
+new WOW().init();
+
+
+let btnMenu = document.getElementById("btnMenu");
+let menuButton = document.getElementById("menuButton");
+let btnclose = document.getElementById("btnclose");
+let body = document.getElementById("body");
+let buttonScroll = document.getElementById("buttonScroll");
+
+
+
+
+btnMenu.addEventListener("click", () => {
+    menuButton.classList.remove("hide")
+    body.classList.add("hide")
+})
+
+btnclose.addEventListener("click", () => {
+    menuButton.classList.add("hide");
+    body.classList.remove("hide")
+})
+
+
+window.onscroll = function () {
+    if (window.scrollY >= 300) {
+        buttonScroll.style.opacity = "1";
+    } else {
+        buttonScroll.style.opacity = "0";
+    }
+
+    buttonScroll.onclick = function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    if (window.scrollY >= 100) {
+        btnMenu.style.opacity = "1";
+    } else {
+        btnMenu.style.opacity = "0";
+    }
+
+}
